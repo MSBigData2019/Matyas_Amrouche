8277642febf1b29e9e77b5ec4bc0ee5a88a0b64b
Matyas <matyas.amrouche@telecom-paristech.fr>
2018-10-10 10:47:10 +0200

    Pretiffy output

--- INFMDI721/Lesson2/ReutersCrawling.py
+++ INFMDI721/Lesson2/ReutersCrawling.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+import datetime
 import requests
 import unittest
 from bs4 import BeautifulSoup
@@ -6,8 +7,9 @@ from bs4 import BeautifulSoup
 # Scraping of Reuters to gather financial information about companies
 
 web_prefix = 'https://www.reuters.com/finance/stocks/financial-highlights/'
-companies = ['LVMH', 'DANONE', 'AIR']
+companies = ['LVMH', 'DANO', 'AIR']
 exchange_places = ['.PA']
+now = datetime.datetime.now()
 
 # Récupère le contenu HTML du site cible
 def recup_source_code(link):
@@ -31,34 +33,46 @@ def get_data1(link):
 # Créer un dictionnaire des valeurs recherchées et de leur index dans la page (extrait manuellement)
 def get_sales_and_dividend(data):
     indexOfValues = {}
+    result = {}
     indexOfValues["Sales estimates (in M) Quarter Ending Dec-18"] = 0
     indexOfValues["Dividend Yield of company"] = 67
     indexOfValues["Dividend Yield of sector"] = 69
     indexOfValues["Dividend Yield of industry"] = 68
 
     for i in indexOfValues.keys():
-        print(i, ":", data[indexOfValues.get(i)])
+        result[i] = data[indexOfValues.get(i)]
 
+    return result
 
-def get_shares_prices_and_percentage(link):
+# Récupère le prix de l'action
+def get_share_price(link):
     page_content = recup_source_code(link)
-    data_value = []
 
     data = page_content.findAll('span', {'style' : 'font-size: 23px;'})
-    print(data[0].text)
+    return  data[0].text.split("\t")[len(data[0].text.split("\t"))-1]
 
-    data = page_content.findAll('span', {'class' : 'pos'})
-    print(data[1].text)
 
-    return data_value
+def get_share_percentage(link):
+    page_content = recup_source_code(link)
+
+    # Variation peut pos ou neg => 2 différentes balises à prendre en compte
+    if len(page_content.findAll('span', {'class' : 'pos'})) == 0:
+        data = page_content.findAll('span', {'class': 'neg'})
+    else:
+        data = page_content.findAll('span', {'class' : 'pos'})
+    return data[1].text.strip(" \n").strip(" \t")
+
 
 
 def main() :
-    link = web_prefix+companies[0]+exchange_places[0]
-    #print("Data for", companies[0], "on the", exchange_places[0], "exchange place")
 
-    #get_sales_and_dividend(get_data1(link))
-    print(get_shares_prices_and_percentage(link))
+    for i in range(0,len(companies)):
+        link = web_prefix + companies[i] + exchange_places[0]
+        print("Fiancial performaces data for", companies[i], "on the", exchange_places[0], "exchange place")
+        for s in get_sales_and_dividend(get_data1(link)).keys():
+            print(s + " : " + get_sales_and_dividend(get_data1(link)).get(s))
+        print("Share price on the " + now.strftime("%Y-%m-%d %H:%M") + " : " + get_share_price(link) + " €")
+        print("Share change % on the " + now.strftime("%Y-%m-%d %H:%M") + " : " +get_share_percentage(link))
 
 if __name__ == '__main__':
     main()
